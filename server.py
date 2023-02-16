bfb03dff6c4e1d3cceb02ba0f907b7015b3cf960
kinyarasam <skinyara.30@gmail.com>
2023-02-16 20:57:32 +0300

    Server implementation using the asyncio method for efficiency.

--- server.py
+++ server.py
@@ -2,31 +2,66 @@
 """TCP server that accepts and holds a maximum of N clients
 """
 import socket
-import random
+import asyncio
 
 
 # Standard loopback interface address (localhost)
 HOST = socket.gethostbyname(socket.gethostname())
 # port to listen on (non-priveleged ports are > 1023)
-PORT = 5010
+PORT = 5040
+BUFF_SIZE = 1024
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-    server.bind((HOST, PORT))
-    server.listen()
-    print("[+] OK: Server started on port: {}".format(PORT))
+async def handle_client(
+        reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+    ) -> None:
+    """
+    ###
 
-    print("[+] OK: Listening . . .")
+    Args:
+        reader (asyncio.StreamReader): ###
+        writer (asyncio.StreamWriter): ###
 
-    connection, address = server.accept()
+    Returns:
+        None.
+    """
+    data = None
+
+    while data != 'quit':
+        data = await reader.read(BUFF_SIZE)
+        message = data.decode()
+        addr, port = writer.get_extra_info("peername")
+        print(f'Received the message from: {addr} {port} {message!r}')
+
+        writer.write(data)
+        await writer.drain()
+
+    writer.close()
+    await writer.wait_closed()
 
 
-    with connection:
-        print("\t[+] Connected by {}".format(address))
-        while True:
-            data = connection.recv(1024)
-            if not data:
-                print("\t[-] No Data Found")
-                break
+async def run_server() -> None:
+    """
 
-            connection.sendall(data)
+    Returns:
+        Nothing.
+    """
+    server = await asyncio.start_server(handle_client, HOST, PORT)
+
+    async with server:
+        print("Listening . . .")
+        await server.serve_forever()
+
+        # tasks = []
+
+        # for client in range(10):
+            # try:
+                # asyncio.create_task(handle_client(*asyncio.open_connection(sock=client)))
+                # await asyncio.gather(*tasks)
+            # except Exception as e:
+                # print("[{}] {}".format(e.__class__.__name__, e))
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_server())
